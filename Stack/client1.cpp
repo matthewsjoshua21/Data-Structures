16e636edd7224c9910586d749a67ee9d3f1a37e9
matthewsjoshua21 <matth039@cougars.csusm.edu>
2020-07-22 13:27:03 -0700

    adjusted stacktest

--- Stack/client1.cpp
+++ Stack/client1.cpp
@@ -1,15 +1,3 @@
-//CS311 Yoshii
-//INSTRUCTION:
-//Look for ** to complete this program (Use control-S)
-//The output should match my hw1stackDemo.out 
-
-//=========================================================
-//HW#: HW1P1 stack application (post-fix evaluation)
-//Your name: Joshua Matthews
-//Complier:  g++ on empress
-//File type: client program client1.cpp
-//===========================================================
-
 using namespace std;
 #include <iostream>
 #include <stdlib.h>
@@ -22,29 +10,29 @@ int main()
 {
   stack postfixstack;  // integer stack
   string expression;   // user entered expression
-  
+
   cout << "type a postfix expression: " ;
   cin >> expression;
-  
+
   int i = 0;  // character position within expression
   char item;  // one char out of the expression
-  
+
   int box1;  // receive things from pop
   int box2;  // receive things from pop
-  
+
   while (expression[i] != '\0')
     {
      try
        {
 	 item = expression[i];  // current char
-	 
+
 	 // ** do all the steps in the algorithm given in Notes-1
-	
-	 
+
+
        } // this closes try
-      // Catch exceptions and report problems and quit the program now (exit(1)). 
+      // Catch exceptions and report problems and quit the program now (exit(1)).
       // Error messages describe what is wrong with the expression.
-      catch (stack::Overflow) 
+      catch (stack::Overflow)
 	{** }
       catch (stack::Underflow)
 	{** }
@@ -54,11 +42,11 @@ int main()
       // go back to the loop after incrementing i
       **
     }// end of while
-  
- // After the loop successfully completes: 
+
+ // After the loop successfully completes:
  // Pop the result and show it.
    **
- // If anything is left on the stack, an incomplete expression 
+ // If anything is left on the stack, an incomplete expression
  // was found so inform the user.
    **
 
